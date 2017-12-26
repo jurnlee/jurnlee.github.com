a2069b99f68281c7656d93151097ed9ecda33b06
jurnlee <jurnlee@sina.com>
2017-12-26 23:00:16 +0800

    adjust index

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@
             window.setTimeout(function () {
                 $body.removeClass('is-loading');
                 typed = new Typed(".b-title", {
-                    strings: ["^500 嗨^1000，屏幕那一端的你，","^500 这里是久礼先生的闲暇世界，^500 可能会分享^1000","技术、感动、生活^1000。"],
+                    strings: ["^500 嗨^1000，屏幕那一端的你，","^500 这里是久礼先生的后花园^1000","技术、感动、生活^1000。"],
                     typeSpeed: 100,
                     backSpeed: 30,
                     backDelay: 500
